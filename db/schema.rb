56cee38599bf1fd0b5a508fd601c5de741cbc711
akaneotake <akane1033@gmail.com>
2025-01-11 16:38:13 +0700

    Create migration file AddAttributesToTasks & Add 2 columns 'content''completed'

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_01_11_083229) do
+ActiveRecord::Schema.define(version: 2025_01_11_083920) do
 
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "content"
+    t.boolean "completed", default: false
   end
 
 end
