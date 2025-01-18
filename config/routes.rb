010c81e6c7d42017dd778a2b5165b1acb87a936b
akaneotake <akane1033@gmail.com>
2025-01-18 14:59:27 +0700

    Add the endpointt GET/tasks by define route & add method

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path' => 'homepage#index'
+  get 'tasks' => 'tasks#index'
 end
